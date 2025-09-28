82d6cc9ddf67f4e8eacf1bdf4fb742bac6c5883c
Fadii77 <fadi.mohamed-v@collectivexp.io>
2025-09-29 01:00:21 +0300

    revert changes back

--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://io-tech-backend-1.onrender.com'),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
