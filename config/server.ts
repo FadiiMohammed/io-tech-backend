b9f1719f32c8ffe6ad4bc98063d587dcc70f98cf
Fadii77 <fadi.mohamed-v@collectivexp.io>
2025-09-29 00:55:26 +0300

    adding url to server

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,10 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  url: env('PUBLIC_URL', 'https://io-tech-backend-1.onrender.com'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
   },
 });
